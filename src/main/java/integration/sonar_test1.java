88c046f0fca15bd6edb442dd72b25e290d4fb0bf
AkanshA0 <akkansha01@gmail.com>
2020-08-21 13:04:29 +0530

    first commit

---
+++ src/main/java/integration/sonar_test1.java
@@ -0,0 +1,14 @@
+package integration;
+
+public class App 
+{
+    private int n=10;
+    public void f1(int n1)
+    {
+        n1=20;
+    }
+        public static void main(String[] args) {
+            System.out.println("Hello sonarqube!");
+    
+        }
+}
